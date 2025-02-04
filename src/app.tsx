95174ccc4e2c741a2a46b3c74b8178eb2898fd33
Abhishek Patawari <abhishekpatawari@Abhisheks-MacBook-Air-2.local>
2025-02-05 04:18:27 +0530

    started again

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,14 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import Navbar from "./components/Navbar";
-
+import { FiInstagram, FiLinkedin, FiFacebook,  FiGithub, FiTwitter } from "react-icons/fi";
 gsap.registerPlugin(useGSAP);
 
 const App = () => {
-  const [sticky, setSticky] = useState(false); // Navbar scroll state
+
   const [nav, setNav] = useState(false); // Toggle state for navbar menu
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setSticky(window.scrollY > 44);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   const container = useRef(null);
   const deerRef = useRef(null);
@@ -32,8 +23,8 @@ const App = () => {
       );
       const tl1 = gsap.timeline();
       const tl2 = gsap.timeline();
-      tl1.to('.rotar',{marginBottom:'0px'}).to('.rotar',{rotate:45}).duration(0.5);
-      tl2.to('.rotar-1',{marginTop:'0px'}).to('.rotar-1',{rotate:-45}).duration(0.5);
+      tl1.to('.rotar',{marginBottom:'0px',transformOrigin:"60% 50%"}).to('.rotar',{rotate:-45}).duration(0.5);
+      tl2.to('.rotar-1',{marginTop:'0px',transformOrigin:"50% 50%"}).to('.rotar-1',{rotate:45}).duration(0.5);
 
 
     } else {
@@ -53,22 +44,47 @@ const App = () => {
     gsap.fromTo(
       ".something",
       { clipPath: "circle(0% at bottom)", backgroundColor: "white" },
-      { clipPath: "circle(150% at bottom)", backgroundColor: "black", duration: 3 }
+      { clipPath: "circle(150% at bottom)", backgroundColor: "black", duration: 1 }
     );
   }, { scope: container });
 
   return (
     <div ref={container} className="">
-      <Navbar nav={nav} sticky={sticky} handleClick={handleClick} />
+      <Navbar nav={nav} handleClick={handleClick} />
       {/* Fixed full-screen menu */}
       <div
         ref={deerRef}
-        className="navigation-bar bg-white fixed top-0 left-0 w-full h-screen z-10 scale-y-0 opacity-0"
+        className="sitemap bg-white fixed top-0 left-0 w-full h-screen z-10 scale-y-0 opacity-0 flex justify-center items-center px-20"
       >
-        
+        <div className="w-full h-50">
+          <div>
+            <div  className="text-anime md:text-5xl fader">HOME</div>
+            <div  className="text-anime md:text-5xl">WORKS</div>
+            <div  className="text-anime md:text-5xl">INFO</div>
+            <div  className="text-anime md:text-5xl">CONTACT</div>
+          </div>
+          <div className="w-full h-1 bg-black"></div>
+          <div className="socials mt-5 flex justify-around">
+            <div className=""><FiInstagram size={25}/></div>
+            <div className=""><FiLinkedin size={25}/></div>
+            <div className=""><FiTwitter size={25}/></div>
+            <div className=""><FiGithub size={25}/></div>
+            <div className=""><FiFacebook size={25}/></div>
+          </div>
+        </div>
       </div>
-      <div className="bg-black h-screen something"></div>
+
+      <div className="bg-black h-screen something text-white flex justify-center items-center text-5xl">
+        <div>
+         <div className="font-extrabold tracking-tighter"> <span>Hi !</span> <span>I'm</span> <span>Abhishek</span> </div>
+         <div className="text-2xl text-right">description container</div>
+         <div className="font-extrabold tracking-tighter"> FULL STACK DEV</div>
+         <div className="text-2xl"> description container</div>
+        </div>
+      </div>
       <div className="bg-blue-600 h-screen scene"></div>
+      <div className="design-process"></div>
+      <div className="footer"></div>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 
 
-const Navbar = ({sticky, handleClick,nav}: {sticky: boolean; handleClick: () => void;nav:boolean}) => {
+const Navbar = ({handleClick,nav}: { handleClick: () => void;nav:boolean}) => {
 
 
   return (
     
-   <div className={` w-full ${sticky? "sticky" :  "absolute"} top-0 py-5 z-30`}>
+   <div className={` w-full fixed top-0 py-5 z-30`}>
       <div className="flex justify-between mx-10 md:mx-40">
         <div className={`text-3xl font-bold font-condensed scale-y-200 ${nav ? 'text-black':'text-white'}`}>IHBA</div>
         <div onClick={handleClick} className="min-h-full w-10 flex flex-col items-center justify-center">
